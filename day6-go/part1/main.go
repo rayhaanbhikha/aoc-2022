6efd6e64498e40afd98e6da2c0f15f087fac63b2
Rayhaan Bhikha <Rayhaanbhikha3@hotmail.com>
2022-12-06 21:10:07 +0000

    day6 done.

---
+++ day6-go/part1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func uniqueChars(input string) bool {
+	charsSeen := make(map[int32]struct{})
+	for _, char := range input {
+		if _, ok := charsSeen[char]; ok {
+			return false
+		}
+		charsSeen[char] = struct{}{}
+	}
+	return true
+}
+
+func parse(input string) int {
+	charSize := 4
+	for i := charSize; i <= len(input); i++ {
+		chars := input[i-charSize : i]
+		if uniqueChars(chars) {
+			return i
+		}
+	}
+	return 0
+}
+
+func main() {
+	data, _ := ioutil.ReadFile("../input")
+	fmt.Println(parse(string(data)))
+}
