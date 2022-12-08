31e9f7cd1d1b4e3db67222b8419397c83dd30e5b
Rayhaan Bhikha <Rayhaanbhikha3@hotmail.com>
2022-12-08 13:21:00 +0000

    day7 done.

---
+++ day7-go/part1/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type FileType int
+
+const (
+	FILE FileType = iota
+	DIR
+)
+
+type File struct {
+	name     string
+	size     int
+	fileType FileType
+}
+
+func NewFile(name string, size int, fileType FileType) *File {
+	return &File{name: name, size: size, fileType: fileType}
+}
+
+type Node struct {
+	val      *File
+	prev     *Node
+	children []*Node
+}
+
+func (n *Node) Add(item *File) {
+	node := &Node{val: item, prev: n}
+	n.children = append(n.children, node)
+}
+
+func (n *Node) Next(name string) *Node {
+	if name == ".." {
+		if n.prev == nil {
+			return n
+		}
+		return n.prev
+	}
+	for _, child := range n.children {
+		if child.val.name == name {
+			return child
+		}
+	}
+	return n
+}
+
+func (n *Node) HasChildren() bool {
+	return len(n.children) != 0
+}
+
+func (n *Node) Walk(cb func(n *Node)) {
+	if !n.HasChildren() {
+		return
+	}
+	for _, child := range n.children {
+		child.Walk(cb)
+	}
+	cb(n)
+}
+
+func main() {
+	data, _ := ioutil.ReadFile("../input")
+	inputs := strings.Split(strings.TrimSpace(string(data)), "\n")
+
+	rootNode := &Node{val: &File{name: "/", fileType: DIR}}
+	currentNode := rootNode
+
+	for _, input := range inputs {
+		if input[0] == '$' {
+			res := strings.Split(input[2:], " ")
+			command := res[0]
+			if command == "cd" {
+				currentNode = currentNode.Next(res[1])
+			}
+			continue
+		}
+		p1, p2, ok := strings.Cut(input, " ")
+		if !ok {
+			continue
+		}
+		var file *File
+		if p1 == "dir" {
+			file = NewFile(p2, 0, DIR)
+		} else {
+			size, err := strconv.Atoi(p1)
+			if err != nil {
+				panic(err)
+			}
+			file = NewFile(p2, size, FILE)
+		}
+		currentNode.Add(file)
+	}
+
+	rootNode.Walk(func(n *Node) {
+		if n.val.fileType == FILE {
+			return
+		}
+		n.val.size = 0
+		for _, child := range n.children {
+			n.val.size += child.val.size
+		}
+	})
+
+	sum := 0
+	rootNode.Walk(func(n *Node) {
+		if n.val.fileType == DIR && n.val.size < 100000 {
+			sum += n.val.size
+		}
+	})
+
+	fmt.Println(sum)
+}
